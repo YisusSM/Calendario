d10be801f7cfe3575452c873a52b6e5f07efd0a8
YisusSM <xxchuchoxd@outlook.com>
2021-08-26 13:54:24 -0500

    implementación de las acciones para los eventos

---
+++ src/actions/events.js
@@ -0,0 +1,13 @@
+import { types } from "../types/types"
+
+
+
+export const eventAddNew = (event) => ({
+    type: types.eventAddNew,
+        led: event
+});
+
+export const eventSetActive = (event) => ({
+    type: types.eventAddNew,
+        led: event
+});
