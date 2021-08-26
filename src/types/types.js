26a5705bb0f5f0dce7468f3224ee9fd233d0621f
YisusSM <xxchuchoxd@outlook.com>
2021-08-26 13:50:51 -0500

    se agrego los types del evento

--- src/types/types.js
+++ src/types/types.js
@@ -5,6 +5,8 @@ export const types = {
     uiOpenModal: '[ui] Open Modal',
     uiCloseModal: '[ui] Close Modal',
 
+    eventAddNew: '[event] Add New',
+    eventSetActive: '[event] Set Active',
 
 
 
